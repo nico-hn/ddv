740fce4bf2389a78c004d042050fa3a83da73b31
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2015-09-26 01:57:55 +0900

    define #dir?

--- lib/ddv.rb
+++ lib/ddv.rb
@@ -6,12 +6,16 @@ def tree(parent_dir, level=0)
   print_dir(File.basename(parent_dir), level)
   Dir.entries(parent_dir).each do |node|
     next if [".", ".."].include? node
-    if File.directory?(File.join(parent_dir, node))
+    if dir?(parent_dir, node)
       tree(File.join(parent_dir, node), level + 1)
     else
       print_file(node, level)
     end
   end
+end
+
+def dir?(parent_dir, node)
+  File.directory?(File.join(parent_dir, node))
 end
 
 def print_dir(dir, level)
