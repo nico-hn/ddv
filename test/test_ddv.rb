8ba45da322bbb389318db867398bc0601d8aef21
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2015-09-27 01:57:49 +0900

    added tests

--- test/test_ddv.rb
+++ test/test_ddv.rb
@@ -1,11 +1,23 @@
 require 'minitest_helper'
 
 class TestDdv < MiniTest::Unit::TestCase
+
+  def in_cur_dir(filename)
+    @cur_dir ||= File.dirname(File.expand_path(__FILE__))
+    File.join(@cur_dir, filename)
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Ddv::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_default_output
+    expected_output = File.read(in_cur_dir("output_data/default.txt"))
+    assert_output(expected_output) { Ddv::DirVisitor.new.tree("test/data") }
+  end
+
+  def test_summarized_2_output
+    expected_output = File.read(in_cur_dir("output_data/summarized_2.txt"))
+    assert_output(expected_output) { Ddv::DirVisitor.new.tree("test/data", 2) }
   end
 end
