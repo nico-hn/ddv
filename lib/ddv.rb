71dcb174d5cf8e74300770c5039ffde7a1b9b0dc
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2015-09-26 11:56:28 +0900

    refactoring #tree: removed "." and ".." prior to the main iteration

--- lib/ddv.rb
+++ lib/ddv.rb
@@ -4,9 +4,8 @@ require 'fileutils'
 
 def tree(parent_dir, level=0)
   print_dir(File.basename(parent_dir), level)
-  nodes = Dir.entries(parent_dir)
+  nodes = Dir.entries(parent_dir) - [".", ".."]
   sort_by_node_type(nodes, parent_dir).each do |node|
-    next if [".", ".."].include? node
     if dir?(parent_dir, node)
       tree(File.join(parent_dir, node), level + 1)
     else
