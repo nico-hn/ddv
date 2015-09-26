873dfd0af440b51fc710190c64c8ae00de757077
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2015-09-27 02:23:12 +0900

    introduced Ddv::NodePrinter

--- lib/ddv.rb
+++ lib/ddv.rb
@@ -5,12 +5,16 @@ require 'fileutils'
 
 module Ddv
   class DirVisitor
+    def initialize(output=NodePrinter.new)
+      @output = output
+    end
+
     def tree(parent_dir, max_detailed_files_num=nil, level=0)
-      print_dir(File.basename(parent_dir), level)
+      @output.print_dir(File.basename(parent_dir), level)
       nodes = Dir.entries(parent_dir) - [".", ".."]
       dirs = nodes.select {|n| dir?(parent_dir, n) }.sort
       files = (nodes - dirs).sort
-      print_files(files, max_detailed_files_num, level)
+      @output.print_files(files, max_detailed_files_num, level)
       dirs.each do |dir|
         tree(File.join(parent_dir, dir), max_detailed_files_num, level + 1)
       end
@@ -19,7 +23,9 @@ module Ddv
     def dir?(parent_dir, node)
       File.directory?(File.join(parent_dir, node))
     end
-    
+  end
+
+  class NodePrinter
     def print_files(files, max_detailed_files_num, level)
       if max_detailed_files_num and files.size > max_detailed_files_num
         print_files_summary(files)
