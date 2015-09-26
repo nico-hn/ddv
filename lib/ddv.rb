ab6783a5d89ef75f79c51383bfaadf0a6964dd6b
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2015-09-26 12:23:58 +0900

    removed unused methods

--- lib/ddv.rb
+++ lib/ddv.rb
@@ -15,16 +15,6 @@ def dir?(parent_dir, node)
   File.directory?(File.join(parent_dir, node))
 end
 
-def dir_file_order(parent_dir, node)
-  dir?(parent_dir, node) ? 1 : 0
-end
-
-def sort_by_node_type(nodes, parent_dir)
-  nodes.map {|n| [dir_file_order(parent_dir, n), n] }.
-    sort {|x, y| x <=> y }.
-    map {|n| n[1] }
-end
-
 def print_dir(dir, level)
   print "  " * level
   puts "[#{dir}]"
