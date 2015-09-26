50daae0d61fffa1adff95d0cc555d78a14956ec1
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2015-09-26 21:08:19 +0900

    added "max_detail_files_num" option

--- lib/ddv.rb
+++ lib/ddv.rb
@@ -2,22 +2,31 @@
 
 require 'fileutils'
 
-def tree(parent_dir, level=0)
+def tree(parent_dir, max_detailed_files_num=nil, level=0)
   print_dir(File.basename(parent_dir), level)
   nodes = Dir.entries(parent_dir) - [".", ".."]
   dirs = nodes.select {|n| dir?(parent_dir, n) }.sort {|x, y| x <=> y }
   files = (nodes - dirs).sort {|x, y| x <=> y }
-  files.each {|file| print_file(file, level) }
-  dirs.each {|dir| tree(File.join(parent_dir, dir), level + 1) }
+  if max_detailed_files_num and files.size > max_detailed_files_num
+    print_files_summary(files)
+  else
+    puts
+    files.each {|file| print_file(file, level) }
+  end
+  dirs.each {|dir| tree(File.join(parent_dir, dir), max_detailed_files_num, level + 1) }
 end
 
 def dir?(parent_dir, node)
   File.directory?(File.join(parent_dir, node))
 end
 
+def print_files_summary(files)
+  puts " => #{files.size} files"
+end
+
 def print_dir(dir, level)
   print "  " * level
-  puts "[#{dir}]"
+  print "[#{dir}]"
 end
 
 def print_file(file, level)
